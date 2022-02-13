6313cb7292758a731e2a531d647039df9b051c85
Laurent Bergeron <laurent.bergeron4@gmail.com>
2022-02-13 12:24:14 -0700

    test

--- html/static/script.js
+++ html/static/script.js
@@ -1,16 +1,16 @@
-// $(window).scroll(function() {
-//     if ($(window).scrollTop() < 1700) {
-//         $(".background").addClass("img1")
-//         $(".background").removeClass("img2")
-//         $(".background").removeClass("img3")
-//     } else if ($(window).scrollTop() < 4000) {
-//         $(".background").addClass("img2")
-//         $(".background").removeClass("img1")
-//         $(".background").removeClass("img3")
-//     } else {
-//         $(".background").addClass("img3")
-//         $(".background").removeClass("img1")
-//         $(".background").removeClass("img2")
-//     }
-//   }
-// );
+$(window).scroll(function() {
+    if ($(window).scrollTop() < 1700) {
+        $(".background").addClass("img1")
+        $(".background").removeClass("img2")
+        $(".background").removeClass("img3")
+    } else if ($(window).scrollTop() < 4000) {
+        $(".background").addClass("img2")
+        $(".background").removeClass("img1")
+        $(".background").removeClass("img3")
+    } else {
+        $(".background").addClass("img3")
+        $(".background").removeClass("img1")
+        $(".background").removeClass("img2")
+    }
+  }
+);
