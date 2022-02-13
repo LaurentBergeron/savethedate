dcfe1de9d6f1a8c14e7d2b6b829f4cbdcec82b00
Laurent Bergeron <laurent.bergeron4@gmail.com>
2022-02-12 17:03:50 -0700

    improve dynamic background

--- html/static/script.js
+++ html/static/script.js
@@ -1,9 +1,16 @@
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 800) {
+    if ($(window).scrollTop() < 1200) {
+        $("body").addClass("img1")
+        $("body").removeClass("img2")
+        $("body").removeClass("img3")
+    } else if ($(window).scrollTop() < 3000) {
+        $("body").addClass("img2")
+        $("body").removeClass("img1")
+        $("body").removeClass("img3")
+    } else {
         $("body").addClass("img3")
-        $("body").removeClass("img4")
-    }else{
-    $("body").addClass("img4")
-     $("body").removeClass("img3")
+        $("body").removeClass("img1")
+        $("body").removeClass("img2")
     }
-  });
+  }
+);
